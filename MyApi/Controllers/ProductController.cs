5f3b4904aff75a89996bce903acf6cc954f3048d
benadidafouzia <benadidafouzia@gmail.com>
2023-10-02 16:42:24 +0300

    product endpoint added

---
+++ MyApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly List<Product> _products = new List<Product>(); // Replace this with a database connection
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_products);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Product product)
+    {
+        // Add validation logic if necessary
+        _products.Add(product);
+        return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+    }
+
+}
